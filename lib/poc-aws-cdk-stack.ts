8aa5046dab93fe0433305efcbf10fad11a3c773a
Fabio <fflucas@hotmail.com>
2021-12-06 23:57:14 -0300

    constructing a s3 bucket ez =)

--- lib/poc-aws-cdk-stack.ts
+++ lib/poc-aws-cdk-stack.ts
@@ -1,16 +1,15 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps, RemovalPolicy } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { aws_s3 as s3 } from "aws-cdk-lib";
 
 export class PocAwsCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'PocAwsCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new s3.Bucket(this, "MyFirstBucket", {
+      versioned: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
   }
 }
